3ea001cc77ff19c58de7cb9c29ad6906297c0532
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-07-05 22:23:41 +0300

    .

--- sendmessage.js
+++ sendmessage.js
@@ -1,27 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const form = document.getElementById('form');
-//   console.log(form);
-//   form.addEventListener('submit', formSend);
-
-//   async function formSend(e) {
-//     e.preventDefault();
-//     let formData = new FormData(form);
-//     console.log(formData);
-
-//     let response = await fetch('sendmail.php', {
-//       method: 'POST',
-//       body: formData,
-//     });
-//     if (response.ok) {
-//       let result = await response.json();
-//       alert(result.message);
-//       form.reset();
-//     } else {
-//       alert('Error');
-//     }
-//   }
-// });
-
 const form = document.querySelector('.form');
 form.addEventListener('submit', function (e) {
   e.preventDefault();
